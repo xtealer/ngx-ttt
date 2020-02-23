e2cddc2e749e511cb970b49a0aea11064adede3d
Enrique Shunnar <xtealer00@gmail.com>
2020-02-23 07:31:08 -0500

    update default serve route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { GameComponent } from './game/game.component';
 import { FormsModule } from '@angular/forms';
+// DEFAULT APP ROUTE -> '/'
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  // OVERRIDE DEFAULT SERVE ROUTE OF APP TO '/xo'
+  providers: [{ provide: APP_BASE_HREF, useValue: '/xo' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
